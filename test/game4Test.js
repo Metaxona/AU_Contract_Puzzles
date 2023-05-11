d5f3f9fb35bc3e539287ac4611204d175ca6c8f1
Metaxona <metaxonadev@gmail.com>
2023-05-12 04:18:53 +0800

    inital commit: ALL TEST PASSED

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,14 +6,17 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const [signer, signer2] = await ethers.getSigners();
+
+    return { game, signer, signer2 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer, signer2 } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    await game.connect(signer).write(signer2.address)
 
-    await game.win();
+    await game.connect(signer2).win(signer.address);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
